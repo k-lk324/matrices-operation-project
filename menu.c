41d799ae37cf05fa58c608d3fa1d66efbb991155
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-03 18:03:09 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+
+//προβολή αρχικού μενού και επιστρέφει την επιλογή
+int prime_menu(){
+
+    //
+
+    printf("ΠΡΑΞΕΙΣ ΠΙΝΑΚΩΝ\n\n\
+           1.Δημιουργία συστυχίας\n\
+           2.Προβολή διαθέσιμων πινάκων\n\
+           3.Πράξεις πινάκων\n\
+           4.Πράξεις διανυσμάτων\n\n");
+
+    printf("Επιλογή: ");
+    int choice;
+    scanf("%d", &choice);
+
+    return choice;
+
+}
+
+
+int main()
+{
+    enum {create_matrix, show_matrix,
+        matrix_operations, vector_operarations};
+
+    switch(prime_menu()) {
+
+        case create_matrix:
+            //δημιουργια πίνακα
+            break;
+        case show_matrixes:
+            //προβολή πίνακα
+            break;
+        case matrix_operations;
+            //πραξεις πινάκων
+            break;
+        case vector_operations;
+            //πράξεις διανυσμάτων
+            break;
+
+        default:
+
+
+    }
+
+    return 0;
+}
 

2763affaff6a9ea0570038e54440213ae119b973
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-06 17:46:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,36 +5,46 @@
 #define ROWS_MAX 100
 #define COLS_MAX 100
 #define NOT_DEBUG
+#define NOT_DEBUGSTR
 
+char *prime_menu = "\tΠΡΑΞΕΙΣ ΜΕΤΑΞΥ ΠΙΝΑΚΩΝ\n\n1.Δημιουργία συστυχίας\n2.Προβολή διαθέσιμων συστοιχιών\n3.Πράξεις πινάκων\n4.Πράξεις διανυσμάτων\n5.Διαγραφή συστυχίας\n6.Έξοδος\n\n";
 
 struct matrix
 {
-    float mat[ROWS_MAX][COLS_MAX];
+    double mat[ROWS_MAX][COLS_MAX];
     int rows,cols;
 };
+
+//ζητάει απο χρήστη πίνακα: διαστάσεις και στοιχεία
 struct matrix getmatrix();
 
-//1η επιλογη
+//δημιουργεί συστυχία και αποθηκεύει σε txt file
+//επιστρέφει false αν δεν κατάφερει να ανοιξει το αρχείο
+//επιστρέφει true αν εκτελεστεί ο κώδικας κανονικά
 bool save_matrix();
 
-//2η επιλογή
+//προβάλει τους διαθέσιμους πίνακες
 bool show_matrixes();
 
-char *prime_menu = "\tΠΡΑΞΕΙΣ ΠΙΝΑΚΩΝ\n\n1.Δημιουργία συστυχίας\n2.Προβολή διαθέσιμων πινάκων\n3.Πράξεις πινάκων\n4.Πράξεις διανυσμάτων\n\n";
-
-//προβολή αρχικού μενού και επιστρέφει την επιλογή
+//τυπώνει μενού με επιλόγές και ζητάει επιλογή απο τον χρήστη
+//επιστρέφει την επιλογή του χρήστη -1
 int menu(char* menu);
 
+//τυπώνει τα στοιχεία ενος πίνακα επιλογής του χρήστη
 void print_elements();
+
+int delete_matrixName(char* name);
+
+int delete_matrix();
 
 int main()
 {
     enum {create_matrix, show_matrix,
-          matrix_operations, vector_operations
-         };
+          matrix_operations, vector_operations, del, exit};
 
-    //while(char != stop)
-    while(true)
+    bool stop = false;
+
+    while(!stop)
     {
         switch(menu(prime_menu))
         {
@@ -55,6 +65,15 @@ int main()
         case vector_operations:
             //πράξεις διανυσμάτων
             break;
+
+        case del:
+            delete_matrix();
+            break;
+
+        case exit:
+            stop = true;
+            break;
+
         default:
             printf("Η επιλογη που βάλατε δεν υπάρχει\n");
         }
@@ -68,25 +87,24 @@ struct matrix getmatrix()
     struct matrix y;
 
     //διαστάσεις
-    printf("\nΔώσε διαστάσεις\nΓραμμές: ");
+    printf("\nΟρισμός διαστάσεων\nΠροσοχή! Μέγιστες διαστάσεις: 100x100\n\nΓραμμές: ");
     scanf("%d", &y.rows);
     printf("Στήλες: ");
     scanf("%d", &y.cols);
 
     //εισαγωγη στοιχειων
-    printf("\nΔώσε στοιχεία πίνακα χωρισμένα με κένο\n");
+    printf("\nΣυμπλήρωση τιμών:\nΔώσε στοιχεία πίνακα χωρισμένα με κένο αλλαγή γραμμής\n");
     printf ("π.χ. ένας πίνακας 2x3:\n1 0 45.87\n2 5.8 89\n\n");
 
     for(int i=0;i<y.rows;i++)
     {
         for (int j=0; j<y.cols; j++)
-        scanf("%f", &y.mat[i][j]);
+        scanf("%lf", &y.mat[i][j]);
     }
 
     return y;
 }
 
-//δημιουργεί συστυχία και αποθηκεύει σε txt file
 bool save_matrix(){
 
     #ifdef DEBUG
@@ -142,7 +160,6 @@ bool save_matrix(){
     return true;
 }
 
-//τυπώνει τους διαθέσιμους πίνακες
 bool show_matrixes(){
     FILE* AM;
     AM = fopen("available_matrix.txt", "r");
@@ -158,12 +175,9 @@ bool show_matrixes(){
     return true;
 }
 
-
 int menu(char* menu)
 {
-
     printf(prime_menu);
-
     printf("Επιλογή: ");
     int choice;
     fflush(stdin);
@@ -203,4 +217,61 @@ void print_elements()
         }
         fclose (fp);
     }
+}
+
+//διαγράφει το όνομα της συστοιχίας απο το "available_matrix.txt"
+int delete_matrixName(char* name)
+{
+    FILE *fp1, *fp2;
+    char matrixName[50];
+
+    //άνοιγμα αρχειου σε αναγνωση??
+    fp1 = fopen("available_matrix.txt", "r");
+    //άνοιγμα αρχειου σε γραψιμο??
+    fp2 = fopen("copy.txt", "w");
+
+    while ((fscanf(fp1,"%s",matrixName))!= EOF)
+    {//&& ελεγχος οτι δεν ειναι σφάλμα της fscanf
+
+        //εκτος απο την συστυχια για διαγραφη
+        if (strcmp(name,matrixName) != 0)
+            //αντέγραψε ολες τις υπόλοιπες γραμμες στο copy.txt
+            fprintf(fp2, "%s\n",matrixName);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+
+    //διαγραφη αρχικου αρχειου
+    remove("available_matrix.txt");
+
+    //μετονομασια του copy.txt σε available_matrix.txt
+    rename("copy.txt", "available_matrix.txt");
+
+    return 0;
+}
+
+//διαγράφει αρχείο συστοιχίας
+int delete_matrix()
+{
+    char name[50];
+
+    printf ("Εισαγωγη συστυχίας για διαγραφη: ");
+    scanf("%s", name);
+
+    char filename[50];
+    strcpy(filename, name);
+
+    //προσθεσε την καταληξη ".txt"
+    strcat(filename, ".txt");
+
+    if (remove(filename) == 0)
+        printf("Διαγράφτηκε επιτυχως\n");
+    else
+        printf("Αδυναμία διαγραφης\n");
+
+    //για να μην προβάλεται στους διαθέσιμες συστοιχίες
+    delete_matrixName(name);
+
+    return 0;
 }

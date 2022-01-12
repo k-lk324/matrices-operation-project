4d638a2843469b634194c172623cca445d8a9783
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-12 19:36:18 +0200

    Update prakseis_function.c

--- prakseis_function.c
+++ prakseis_function.c
@@ -1,25 +1,80 @@
-struct matrix matrix_result(int operation){
+#define ROWS_MAX 20
+#define COLS_MAX 20
 
-    //δημιουργία συστοιχίας αποτελέσματος
-    struct matrix matrixC;
+struct matrix{
+    int rows,cols;
+    double mat[ROWS_MAX][COLS_MAX];
+};
 
-    //περιπτώσεις πράξεων
-    enum {sum=1, subtraction, multiplication};
-        switch(operation)
-    {
-        case sum:
-            //matrixC = πρόσθεση(matrix("A"), matrix("Β"));
-            break;
-        case subtraction:
-            //matrixC = αφαίρεση(matrix("A"), matrix("Β"));
-            break;
-        case multiplication:
-            //matrixC = πολ/σμος(matrix("A"), matrix("Β"));
-            break;
-        default:
-        printf("Η επίλογη αυτή δεν υπάρχει\n");
+//προσθέτει 2 πίνακες
+//@επιστρέφει structure με τον πίνακα αθροίσματος
+struct matrix sum_matrix(struct matrix A, struct matrix B) {
+
+    struct matrix C;
+
+    if(A.rows == B.rows && A.cols == B.cols) {
+        C.rows = A.rows;
+        C.cols = A.cols;
+
+        for(int i=0; i<A.rows; i++)
+            for(int j=0; j<A.rows; j++)
+                C.mat[i][j]=A.mat[i][j]+ B.mat[i][j];
     }
-  
-  //επιστέφει το αποτέλεσμα
-    return matrixC;
+
+    return C;
+}
+
+//αφαιρεί 2 πίνακες
+//@επιστρέφει structure με τον πίνακα διαφοράς
+struct matrix substraction_matrix(struct matrix A, struct matrix B) {
+    struct matrix C;
+
+    if(A.rows == B.rows && A.cols == B.cols) {
+        C.rows = A.rows;
+        C.cols = A.cols;
+
+        for(int i=0; i<A.rows; i++)
+            for(int j=0; j<A.cols; j++)
+                C.mat[i][j]=A.mat[i][j]- B.mat[i][j];
+    }
+
+    return C;
+}
+
+//πολλαπλασιαζει 2 πίνακες
+//@επιστρέφει structure με τον πίνακα γινόμενο
+struct matrix multiplication_matrix(struct matrix A, struct matrix B) {
+    struct matrix C;
+
+    if(A.cols == B.rows) {
+        int k = A.cols;
+        C.rows = A.rows;
+        C.cols = B.cols;
+
+        int sum = 0;
+        for(int i=0; i<C.rows; i++) {
+            for(int j=0; j<C.cols; j++) {
+                for(int l=0; l<k; l++)
+                    sum += A.mat[i][l]*B.mat[l][j];
+                C.mat[i][j]=sum;
+                sum=0;
+            }
+        }
+    }
+    return C;
+}
+
+//βρίσκει τον ανάστροφο ενός πίνακα
+//@επιστρέφει structure με τον ανάστροφο
+struct matrix traspose(struct matrix A){
+    struct matrix At;
+    At.rows = A.cols;
+    At.cols = A.rows;
+
+    for(int i=0; i<A.rows; i++){
+        for(int j=0; j<A.cols; j++){
+            At.mat[j][i] = A.mat[i][j];
+        }
+    }
+    return At;
 }

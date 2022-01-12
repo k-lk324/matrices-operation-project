d8e3bebb6bc074dff31d137c413acd45a430af59
miliousi <97043049+miliousi@users.noreply.github.com>
2022-01-12 22:32:41 +0200

    Add files via upload

---
+++ esoterikoginomeno.c
@@ -0,0 +1,22 @@
+int main()
+{
+
+double esgin(struct matrix A, struct matrix B)
+
+if(A.rows == B.rows && A.cols == B.cols) {
+
+
+     double p;
+
+        for(int i=0; i<A.rows; i++)
+           {
+                for(int j=0; j<A.cols; j++)
+                    {
+                        p=A.mat[i][j]* B.mat[i][j];
+                        esgin+=p;
+                    }
+            }
+
+        return esgin;
+
+}

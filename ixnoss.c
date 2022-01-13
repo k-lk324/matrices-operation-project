9a04c0cd6e779bceffcb869cc327b87c65c29e0b
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-13 22:03:57 +0200

    Update ixnoss.c

--- ixnoss.c
+++ ixnoss.c
@@ -2,8 +2,11 @@ double ixnos(struct matrix A){
 
      if(A.rows == A.cols ) {
 
-          int trace=0.0;
-          double k =0.0;
+          double trace=0; //θέλουμε να ειναι double ετσι ωστε η τιμη που επιστρεφουμε να ειναι και αυτη double 
+          double k=0; //δεν χρειαζεται να βαλουμε 0.0 γτ ο υπολογιστης το μετατρεπει αυτοματα σε δεκαδικο επειδη γραψαμε απο μπροστα double
+          
+          //αν θεταμε int a = 2.43 τοτε αυτο επιστρεφει το ακεραιο μερος δλδ a=2
+          //αν θέταμε double b = 3.3432 τοτε αυτο επιστρέφει 3.343200 ή αν θέταμε double c = 3 τότε αυτο επιστρέφει 3.000000
 
              for(int i=0; i<A.rows; i++)
                 {
@@ -12,7 +15,7 @@ double ixnos(struct matrix A){
                              if(i==j)
                                  k=A.mat[i][j];
                              else
-                                 k=0.0;
+                                 k=0;
 
                              trace+=k;
                          }

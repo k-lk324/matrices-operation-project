4b567fc1a3de329ffacda2389ba40063ab33169a
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-13 15:48:54 +0200

    Update esoterikoginomeno.c

--- esoterikoginomeno.c
+++ esoterikoginomeno.c
@@ -3,7 +3,7 @@
 double esgin(struct matrix A, struct matrix B){
 
      if(A.rows == B.rows && A.cols == B.cols) {
-     double p;
+     double p=0;
 
         for(int i=0; i<A.rows; i++)
                 for(int j=0; j<A.cols; j++)

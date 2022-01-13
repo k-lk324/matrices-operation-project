2f702964c6d27ca16b67e9172e2425088cd2b3ce
k324ece <97043061+k324ece@users.noreply.github.com>
2022-01-13 15:34:18 +0200

    Update prakseis_function.c

--- prakseis_function.c
+++ prakseis_function.c
@@ -78,3 +78,65 @@ struct matrix traspose(struct matrix A){
     }
     return At;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#define ROWS_MAX 1000
+#define COLS_MAX 1000
+#define DEBUG
+
+//υπολογίζει τον προσαρτημένο ενος πίνακα
+//@επιστρέφει structure με τον προσαρτημένο
+struct mattix adj(struct matrix A){
+    struct matrix y = define_matrix();
+    y.cols = A.cols;
+    y.rows = A.rows;
+
+    for (int i=0; i<y.rows; i++){
+        for (int j=0; j<y.cols; j++){
+            y.mat[i][j]= pow(-1,(i+1)+(j+1)) * A.mat[i][j];
+        }
+    }
+    return traspose(y);
+
+}
+
+//υπολογιζει τον αντιστροφο ενος πίνακα
+//@επιστρέφει structure με τον αντίστροφο
+struct matrix inverse(struct matrix A){
+    struct matrix X = define_matrix();
+    X.cols = A.rows;
+    X.rows = A.cols;
+
+   if (det(A) == 0){
+        puts("Ο πίνακας δεν ειναι αντιστρέψιμος");
+        X.invalid = true;
+    }
+    else {
+        for(int i=0; i<X.rows; i++)
+            for(int j=0; j<X.rows; j++)
+                X.mat[i][j] = (1/det(A)) * adj(A).mat[i][j];
+    }
+    return X;
+}
+
+
+//υπολογίζει εναν τετραγωνικο πίνακα εις την κάποια δύναμη
+//@επιστρεφει τον εκθετικο
+struct matrix exp(struct matrix A, int power){
+    struct matrix y = A;
+
+    if(A.cols == A.rows) {
+        for(int i=0; i<power; i++)
+            y = multiplication_matrix(y,A);
+    }
+    else{
+        y.invalid = true;
+    }
+
+  return y;
+}
